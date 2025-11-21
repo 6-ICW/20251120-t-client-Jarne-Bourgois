7270073bb7d3450cc577850dcb0c790297526eed
jarnebourgois <Jarne.Bourgois@student.tsaam.be>
2025-11-21 15:50:10 +0100

    commit 2

--- script/script.js
+++ script/script.js
@@ -1,3 +1,47 @@
 /**
- * 
+ *
  */
+
+const kinderen = document.querySelector("#kinderen");
+const geschenkenlijst = document.querySelector("#geschenkenlijst");
+const options = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({}),
+};
+
+fetch("https://o-apiandclient-render.onrender.com/kinderen")
+  .then((result) => result.json())
+  .then((data) => {
+    console.log(data);
+    data.forEach((element) => {
+      const newOption = document.createElement("option");
+      newOption.innerHTML = element.voornaam + element.achternaam;
+      newOption.value = element.id;
+      kinderen.appendChild(newOption);
+    });
+  });
+
+kinderen.addEventListener("change", (e) => {
+  fetch("https://o-apiandclient-render.onrender.com/kinderen/" + e.target.value)
+    .then((result) => result.json())
+    .then((data) => {
+      console.log(data);
+      fetch(
+        "https://o-apiandclient-render.onrender.com/geschenken/" +
+          data.geschenkId
+      )
+        .then((result) => result.json())
+        .then((geschenk) => {
+          console.log(geschenk);
+          geschenk.forEach((item) => {
+            const newLi = document.createElement("li");
+            newLi.innerHTML = item.naam;
+
+            geschenkenlijst.appendChild(newLi);
+          });
+        });
+    });
+});
